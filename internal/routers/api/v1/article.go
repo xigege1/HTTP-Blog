c7b61fd736f9ab6af136cba3ef938873395b4b42
xigege1 <33652445+xigege1@users.noreply.github.com>
2022-04-05 19:20:37 +0800

    Initial commit

---
+++ internal/routers/api/v1/article.go
@@ -0,0 +1,22 @@
+package v1
+
+import (
+	"HTTP-Blog/pkg/app"
+	"HTTP-Blog/pkg/errcode"
+	"github.com/gin-gonic/gin"
+)
+
+type Article struct{}
+
+func NewArticle() Article {
+	return Article{}
+}
+
+func (a Article) Get(c *gin.Context) {
+	app.NewResponse(c).ToErrorResponse(errcode.ServerError)
+	return
+}
+func (a Article) List(c *gin.Context)   {}
+func (a Article) Create(c *gin.Context) {}
+func (a Article) Update(c *gin.Context) {}
+func (a Article) Delete(c *gin.Context) {}
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"HTTP-Blog/global"
+	"HTTP-Blog/internal/model"
+	"HTTP-Blog/internal/routers"
+	"HTTP-Blog/pkg/logger"
+	"HTTP-Blog/pkg/setting"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"log"
+	"net/http"
+	"time"
+)
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
+	}
+	err = setupLogger()
+	if err != nil {
+		log.Fatalf("init.setupLogger err: %v", err)
+	}
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setupLogger() error {
+	global.Logger = logger.NewLogger(&lumberjack.Logger{
+		Filename:  global.AppSetting.LogSavePath + "/" + global.AppSetting.LogFileName + global.AppSetting.LogFileExt,
+		MaxSize:   600,
+		MaxAge:    10,
+		LocalTime: true,
+	}, "", log.LstdFlags).WithCaller(2)
+
+	return nil
+}
+
+// @title 博客系统
+// @version 1.0
+// @description Go 语言编程之旅：一起用 Go 做项目
+// @termsOfService https://github.com/go-programming-tour-book
+func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":" + global.ServerSetting.HttpPort,
+		Handler:        router,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
